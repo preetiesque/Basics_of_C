78d4869b63a083d2a8c55f7626e6a74c92790206
Preeti Chaudhari <preetischaudhari1201@gmail.com>
2022-03-26 11:37:33 +0530

    Storage Classes

---
+++ Questions/Question_C_loop_redefining.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int k = 0;
+    int j = 0;
+    while (k==j){
+        int k;
+        for (k = 0 ; k < 3 ; k ++)
+            j = j + k;
+        }
+        k ++;
+        printf("%d\n",j);
+        printf("%d\n",k);
+        
+}
+
---
+++ Storage Classes/auto_storage_class_compile_error.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+auto int var = 200;
+
+int main()
+{
+    printf("The value of var is %d",var);
+}
+//auto storage class is exclusively for declaring local variables and not global variables,hence the program gives compilation error
+
