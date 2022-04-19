4be41355cba87598c322a0d185cee1532bffb54e
Preeti Chaudhari <preetischaudhari1201@gmail.com>
2022-04-19 06:43:13 +0530

    double Pointer

---
+++ Pointers/Double Pointers/double_pointer_question.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+rer(int **ptr2 , int **ptr1){
+    int *ii;
+    ii = *ptr2;
+    *ptr2 = *ptr1;
+    *ptr1 = ii;
+    **ptr1*=**ptr2;
+    **ptr2+=**ptr1;
+}
+
+void main()
+{
+   int var1 = 5 , var2 = 10;
+   int *ptr1 = &var1 , *ptr2 = &var2;
+   rer(&ptr1,&ptr2);
+   printf("%d %d",var2,var1);
+}
